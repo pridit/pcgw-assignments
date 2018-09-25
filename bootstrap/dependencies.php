0b010ff92d66b41ce8235ebbbecd4725e4062c4e
Jordan Heale <webmaster@pridit.co.uk>
2018-09-25 23:32:16 +0100

    Timezone change

--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -9,7 +9,7 @@ $container['view'] = function ($c) {
         'debug' => env('APP_DEBUG')
     ]);
 
-    $view->getEnvironment()->getExtension('Twig_Extension_Core')->setTimezone('Europe/London');
+    $view->getEnvironment()->getExtension('Twig_Extension_Core')->setTimezone(date_default_timezone_get());
 
     $view->addExtension(new \Slim\Views\TwigExtension(
         $c['router'],
